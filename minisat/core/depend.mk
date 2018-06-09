e894147fd949826fe66c0f6ae7cf71624ceb4002
Akanksha Paul <39774933+akku1506@users.noreply.github.com>
2018-06-09 18:30:55 +0530

    Add files via upload

---
+++ minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/home/akanksha/ai/l3/minisat/core/Solver.o /home/akanksha/ai/l3/minisat/core/Solver.or /home/akanksha/ai/l3/minisat/core/Solver.od /home/akanksha/ai/l3/minisat/core/Solver.op: /home/akanksha/ai/l3/minisat/core/Solver.cc \
+ /home/akanksha/ai/l3/minisat/mtl/Sort.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h
+/home/akanksha/ai/l3/minisat/core/Main.o /home/akanksha/ai/l3/minisat/core/Main.or /home/akanksha/ai/l3/minisat/core/Main.od /home/akanksha/ai/l3/minisat/core/Main.op: /home/akanksha/ai/l3/minisat/core/Main.cc \
+ /home/akanksha/ai/l3/minisat/utils/System.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/core/Dimacs.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h
---
+++ minisat/simp/depend.mk
@@ -0,0 +1,59 @@
+/home/akanksha/ai/l3/minisat/simp/SimpSolver.o /home/akanksha/ai/l3/minisat/simp/SimpSolver.or /home/akanksha/ai/l3/minisat/simp/SimpSolver.od /home/akanksha/ai/l3/minisat/simp/SimpSolver.op: /home/akanksha/ai/l3/minisat/simp/SimpSolver.cc \
+ /home/akanksha/ai/l3/minisat/mtl/Sort.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/simp/SimpSolver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Queue.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h \
+ /home/akanksha/ai/l3/minisat/utils/System.h
+/home/akanksha/ai/l3/minisat/simp/Main.o /home/akanksha/ai/l3/minisat/simp/Main.or /home/akanksha/ai/l3/minisat/simp/Main.od /home/akanksha/ai/l3/minisat/simp/Main.op: /home/akanksha/ai/l3/minisat/simp/Main.cc \
+ /home/akanksha/ai/l3/minisat/utils/System.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/core/Dimacs.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h \
+ /home/akanksha/ai/l3/minisat/simp/SimpSolver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Queue.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h
+/home/akanksha/ai/l3/minisat/core/Solver.o /home/akanksha/ai/l3/minisat/core/Solver.or /home/akanksha/ai/l3/minisat/core/Solver.od /home/akanksha/ai/l3/minisat/core/Solver.op: /home/akanksha/ai/l3/minisat/core/Solver.cc \
+ /home/akanksha/ai/l3/minisat/mtl/Sort.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h
+/home/akanksha/ai/l3/minisat/core/Main.o /home/akanksha/ai/l3/minisat/core/Main.or /home/akanksha/ai/l3/minisat/core/Main.od /home/akanksha/ai/l3/minisat/core/Main.op: /home/akanksha/ai/l3/minisat/core/Main.cc \
+ /home/akanksha/ai/l3/minisat/utils/System.h \
+ /home/akanksha/ai/l3/minisat/mtl/IntTypes.h \
+ /home/akanksha/ai/l3/minisat/utils/ParseUtils.h \
+ /home/akanksha/ai/l3/minisat/utils/Options.h \
+ /home/akanksha/ai/l3/minisat/mtl/Vec.h \
+ /home/akanksha/ai/l3/minisat/mtl/XAlloc.h \
+ /home/akanksha/ai/l3/minisat/core/Dimacs.h \
+ /home/akanksha/ai/l3/minisat/core/SolverTypes.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alg.h \
+ /home/akanksha/ai/l3/minisat/mtl/Map.h \
+ /home/akanksha/ai/l3/minisat/mtl/Alloc.h \
+ /home/akanksha/ai/l3/minisat/core/Solver.h \
+ /home/akanksha/ai/l3/minisat/mtl/Heap.h
